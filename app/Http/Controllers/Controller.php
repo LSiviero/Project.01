971dfeaf3245d0fe245e55730b745bcb9f4eb4ef
Leonardo Siviero <leonardo.siviero@outlook.com>
2021-04-09 23:50:35 -0300

    Ajustes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Khill\Lavacharts\Lavacharts;
+
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
 }
